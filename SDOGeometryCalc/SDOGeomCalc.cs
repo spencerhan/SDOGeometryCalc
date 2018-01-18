8e9bb7599de9d82fa2963cdcb686667d95295496
Spencer - dev <spencer.default.dev@gmail.com>
2018-01-18 18:05:47 +1300

    finished the Oracle handshaking

--- SDOGeometryCalc/SDOGeomCalc.cs
+++ SDOGeometryCalc/SDOGeomCalc.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.IO;
+﻿using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
 
 namespace SDOGeometryCalc {
     public class SDOGeomCalc : ESRI.ArcGIS.Desktop.AddIns.Button {
+       
         public SDOGeomCalc() {
         }
 
@@ -14,26 +15,46 @@ namespace SDOGeometryCalc {
             //
             ArcMap.Application.CurrentTool = null;
 
-            using (OleDbConnection conn = new OleDbConnection()) {
-                string pass = '';
-                conn.ConnectionString = "Provider=OraOLEDB.Oracle;Data Source =(DESCRIPTION = (ADDRESS_LIST =(ADDRESS = (PROTOCOL = TCP)(HOST = oradbtest)(PORT = 1521)))(CONNECT_DATA = (SERVICE_NAME = test.wairc.govt.nz))); User ID = gis_workspace; Password ="+ pass +";Min Pool Size=10;Connection Lifetime=120;Connection Timeout=60;Incr Pool Size=5; Decr Pool Size=2;Max Pool Size=30;Validate Connection = true"; 
-                OleDbCommand command = conn.CreateCommand();
-                command.CommandText = "Select ROUND(SDO_GEOM.SDO_AREA(geometry, 0.005)/10000, 2) AS area_ha from CMO_OTHER_FEATURES_POLYGON_EVW"; // feature name needs to be dynamic
-                conn.Open();
-                OleDbDataReader reader = command.ExcuteReader();
-                List<string> result = new List<string>();
-                while (reader.read()){
-                    for (int i = 0; i <reader.FieldCount(); i++ ){
-                        result.Add(reader.Item[i]);
-                    }
-                }
-              MessageBox.Show(result.ToString(), "Dumping", MessageBoxButtons.OK);             
-            }
+            #region getting active layer
 
+
+
+            #endregion
+
+            #region step into Oracle
+            //string pass = "";
+            //using (OleDbConnection conn = new OleDbConnection()) {
+            //    conn.ConnectionString = "Provider=OraOLEDB.Oracle;Data Source =(DESCRIPTION = (ADDRESS_LIST =(ADDRESS = (PROTOCOL = TCP)(HOST = oradbtest)(PORT = 1521)))(CONNECT_DATA = (SERVICE_NAME = test.wairc.govt.nz))); User ID = gis_workspace; Password =" + pass + ";Min Pool Size=10;Connection Lifetime=120;Connection Timeout=60;Incr Pool Size=5; Decr Pool Size=2;Max Pool Size=30;Validate Connection = true";
+            //    using (OleDbCommand command = conn.CreateCommand()) {
+            //        command.CommandText = "Select ROUND(SDO_GEOM.SDO_AREA(geometry, 0.005)/10000, 6) AS area_ha from CMO_OTHER_FEATURES_POLYGON_EVW"; // feature name needs to be dynamic
+            //        conn.Open();
+            //        //
+            //        // Section that reads data
+            //        //
+            //        using (OleDbDataReader reader = command.ExecuteReader()) {
+            //            List<string> result = new List<string>();
+            //            while (reader.Read()) {
+            //                for (int i = 0; i < reader.FieldCount; i++) {
+            //                    try {
+            //                        result.Add(reader.GetValue(i).ToString());
+            //                    } catch (System.InvalidCastException e) {
+            //                        result.Add("Invalid Value");
+            //                    }
+
+            //                }
+            //            }
+            //            //  debug
+            //            MessageBox.Show(result.ToString(), "Dumping", MessageBoxButtons.OK);
+            //        }
+            //    }
+
+            //}
+            #endregion
         }
         protected override void OnUpdate() {
             Enabled = ArcMap.Application != null;
         }
+
     }
 
 }
