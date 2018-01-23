2c80f78db8312e966d3d7d031cb96d2f0914d12a
Spencer - dev <spencer.default.dev@gmail.com>
2018-01-23 17:22:15 +1300

    ready for testing

--- SDOGeometryCalc/SDOGeomCalc.cs
+++ SDOGeometryCalc/SDOGeomCalc.cs
@@ -12,7 +12,7 @@ using ESRI.ArcGIS.Editor;
 
 namespace SDOGeometryCalc {
     public class SDOGeomCalc : ESRI.ArcGIS.Desktop.AddIns.Button {
-        private static Boolean labelOff = true; // check label on or not.
+
         public SDOGeomCalc() {
         }
 
@@ -24,53 +24,42 @@ namespace SDOGeometryCalc {
             IMxDocument pMxDoc = (IMxDocument)ArcMap.Application.Document;
             IMap pMapWin = pMxDoc.FocusMap;
             ILayer iLayer = pMxDoc.SelectedLayer;
-
-            if (labelOff) {
+            UID editorUID = new UID();
+            editorUID.Value = "esriEditor.Editor";
+            IEditor3 editor = (IEditor3)ArcMap.Application.FindExtensionByCLSID(editorUID);
+            Boolean editSession = (editor.EditState == esriEditState.esriStateEditing);
+            if (!editSession) {
                 #region getting active layer
                 try {
-                    if (iLayer is IFeatureLayer) {
-                        IFeatureLayer iFLayer = (IFeatureLayer)iLayer;
-                        IFeatureClass iFclass = iFLayer.FeatureClass;
-                        //MessageBox.Show(iFclass.AliasName, "DataSource", MessageBoxButtons.OK);
-                        string dbName = string.Concat(iFclass.AliasName.TakeWhile((c) => c != '.')); // reference: https://stackoverflow.com/questions/1857513/get-substring-everything-before-certain-char
-                        string layerName = iFclass.AliasName.Replace(dbName, "");
-                        string colName;
-                        string sdoFunc;
-                        UID editorUID = new UID();
-                        editorUID.Value = "esriEditor.Editor";
-                        IEditor3 editor = (IEditor3)ArcMap.Application.FindExtensionByCLSID(editorUID);
-                        Boolean editSession = (editor.EditState == esriEditState.esriStateEditing);
-                        esriGeometryType fGeom = iFclass.ShapeType;
-                        if (editSession) {
-                            throw new Exception("Please exit current Editing Session.");
-                        } else {
-                            if (fGeom == esriGeometryType.esriGeometryLine || fGeom == esriGeometryType.esriGeometryPolyline) {
-                                colName = "SHAPE_LENGTH";
-                                sdoFunc = "SDO_LENGTH";
-                                return;
-                            } else if (fGeom == esriGeometryType.esriGeometryPolygon) {
-                                colName = "SHAPE_AREA";
-                                sdoFunc = "SDO_AREA";
-                                return;
-                            } else {
-                                colName = "";
-                                sdoFunc = "";
-                            }
-                            this.getOracleGeometry(dbName, colName, sdoFunc, layerName + "_EVW");// tailing of actual layername
-                            labelOff = false;
-                        }
+                    IGeoFeatureLayer iGeoFLayer = this.featureTest(iLayer);
+                    IFeatureClass iFclass = iGeoFLayer.FeatureClass;
+                    //MessageBox.Show(iFclass.AliasName, "DataSource", MessageBoxButtons.OK);
+                    string dbName = string.Concat(iFclass.AliasName.TakeWhile((c) => c != '.')); // reference: https://stackoverflow.com/questions/1857513/get-substring-everything-before-certain-char
+                    string layerName = iFclass.AliasName.Replace(dbName, "");
+                    string colName;
+                    string sdoFunc;
+                    esriGeometryType fGeom = iFclass.ShapeType;
+                    if (fGeom == esriGeometryType.esriGeometryLine || fGeom == esriGeometryType.esriGeometryPolyline) {
+                        colName = "SHAPE_LENGTH";
+                        sdoFunc = "SDO_LENGTH";
+                        return;
+                    } else if (fGeom == esriGeometryType.esriGeometryPolygon) {
+                        colName = "SHAPE_AREA";
+                        sdoFunc = "SDO_AREA";
+                        return;
                     } else {
-                        throw new InvalidCastException("Please select a layer and check the selected layer is a 'Feature' layer (not a group layer etc.)");
+                        throw new Exception("Not a supported geometry type.");
                     }
+                    this.getOracleGeometry(dbName, colName, sdoFunc, layerName, iGeoFLayer);
                 } catch (Exception ex) {
                     MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK);
                 }
                 #endregion
 
-
             } else {
-                #region removing label
-
+                #region normal labeling
+                IGeoFeatureLayer iGeoFLayer = this.featureTest(iLayer);
+                this.geometryAnnotation(iGeoFLayer);
                 #endregion
             }
 
@@ -79,31 +68,66 @@ namespace SDOGeometryCalc {
             Enabled = ArcMap.Application != null;
         }
 
-        protected void getOracleGeometry(string dbName, string colName, string sdoFunc, string layerName) {
-            string dbPass;
-            if (colName.Equals("")) {
+        protected IGeoFeatureLayer featureTest(ILayer _iLayer) {
+            if (_iLayer is IGeoFeatureLayer) {
+                IGeoFeatureLayer _iGeoFLayer = (IGeoFeatureLayer)_iLayer;
+                return _iGeoFLayer;
+            } else {
+                throw new InvalidCastException("Please select a layer and check the selected layer is a 'Feature' layer (not a group layer etc.)");
+            }
+        }
+
+        protected void geometryAnnotation(IGeoFeatureLayer _iGeoFLayer) {
+            esriGeometryType _fGeom = _iGeoFLayer.FeatureClass.ShapeType;
+            if (_iGeoFLayer.DisplayAnnotation) {
+                if (_fGeom == esriGeometryType.esriGeometryLine || _fGeom == esriGeometryType.esriGeometryPolyline) {
+                    _iGeoFLayer.DisplayField = "GEOMETRY.LEN";
+                } else {
+                    _iGeoFLayer.DisplayField = "GEOMETRY.AREA";
+                }
+            } else {
+                if (_fGeom == esriGeometryType.esriGeometryLine || _fGeom == esriGeometryType.esriGeometryPolyline) {
+                    _iGeoFLayer.DisplayField = "GEOMETRY.LEN";
+                } else {
+                    _iGeoFLayer.DisplayField = "GEOMETRY.AREA";
+                }
+                _iGeoFLayer.DisplayAnnotation = true;
+            }
+            _iGeoFLayer.ShowTips = true;
+        }
+
+        protected void getOracleGeometry(string _dbName, string _colName, string _sdoFunc, string _layerName, IGeoFeatureLayer _iGeoFLayer) {
+            string _dbPass;
+            if (_colName.Equals("")) {
                 throw new Exception("Geometry column name is not found. \n Check layer geometry type (Multiline, line and polygon");
             } else {
-                if (dbName.Equals("GIS_WORKSPACE")) {
-                    dbPass = "";
+                if (_dbName.Equals("GIS_WORKSPACE")) {
+                    _dbPass = "";
                     using (OleDbConnection conn = new OleDbConnection()) {
-                        conn.ConnectionString = "Provider=OraOLEDB.Oracle;Data Source =(DESCRIPTION = (ADDRESS_LIST =(ADDRESS = (PROTOCOL = TCP)(HOST = oradblive)(PORT = 1521)))(CONNECT_DATA = (SERVICE_NAME = live.wairc.govt.nz))); User ID = " + dbName + "; Password =" + dbPass + ";Min Pool Size=10;Connection Lifetime=120;Connection Timeout=60;Incr Pool Size=5; Decr Pool Size=2;Max Pool Size=30;Validate Connection = true";
+                        conn.ConnectionString = "Provider=OraOLEDB.Oracle;Data Source =(DESCRIPTION = (ADDRESS_LIST =(ADDRESS = (PROTOCOL = TCP)(HOST = oradblive)(PORT = 1521)))(CONNECT_DATA = (SERVICE_NAME = live.wairc.govt.nz))); User ID = " + _dbName + "; Password =" + _dbPass + ";Min Pool Size=10;Connection Lifetime=120;Connection Timeout=60;Incr Pool Size=5; Decr Pool Size=2;Max Pool Size=30;Validate Connection = true";
                         using (OleDbCommand command = conn.CreateCommand()) {
-                            command.CommandText = "UPDATE TABLE " + layerName + " SET " + colName + "= SDO_GEOM." + sdoFunc + "(geometry,0.005)";
+                            command.CommandText = "UPDATE TABLE " + _layerName + " SET " + _colName + "= SDO_GEOM." + _sdoFunc + "(geometry,0.005)";
                             conn.Open();
-
-
-                            #region Reading data
-                            command.CommandText = "Select ROUND(SDO_GEOM.SDO_AREA(geometry, 0.005)/10000, 6) AS area_ha from CMO_OTHER_FEATURES_POLYGON_EVW"; // feature name needs to be dynamic
-                            using (OleDbDataReader reader = command.ExecuteReader()) {
-                                List<string> result = new List<string>();
-                                while (reader.Read()) {
-                                    for (int i = 0; i < reader.FieldCount; i++) {
-                                        result.Add(reader.GetValue(i).ToString());
-                                    }
-                                }
+                            command.ExecuteNonQuery();
+                            if (_iGeoFLayer.DisplayAnnotation) {
+                                _iGeoFLayer.DisplayField = _colName;
+                            } else {
+                                _iGeoFLayer.DisplayField = _colName;
+                                _iGeoFLayer.DisplayAnnotation = true;
                             }
-                            #endregion
+                            _iGeoFLayer.ShowTips = true;
+
+
+                            //command.CommandText = "Select" + colName + " from " + layerName + "_EVW"; // feature name needs to be dynamic
+                            //using (OleDbDataReader reader = command.ExecuteReader()) {
+                            //    List<string> result = new List<string>();
+                            //    while (reader.Read()) {
+                            //        for (int i = 0; i < reader.FieldCount; i++) {
+                            //            result.Add(reader.GetValue(i).ToString());
+                            //        }
+                            //    }
+                            //}
+
                         }
                     }
                 } else {
